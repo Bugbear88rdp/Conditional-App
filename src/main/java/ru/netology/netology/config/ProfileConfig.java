206846480619ba28f558488653bb55aeb4c7a4c8
Bugbear88rdp <ershov.k94@gmail.com>
2026-02-21 11:50:05 +0300

    Update ProfileConfig.java

--- src/main/java/ru/netology/netology/config/ProfileConfig.java
+++ src/main/java/ru/netology/netology/config/ProfileConfig.java
@@ -1,4 +1,24 @@
 package ru.netology.netology.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.netology.profile.DevProfile;
+import ru.netology.netology.profile.ProductionProfile;
+import ru.netology.netology.profile.SystemProfile;
+
+@Configuration
 public class ProfileConfig {
+
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "false", matchIfMissing = true)
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
 }
