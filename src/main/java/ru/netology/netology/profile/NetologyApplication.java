d7c6a599411fd6bcd05eb5b8232b87a4f238414f
Bugbear88rdp <ershov.k94@gmail.com>
2026-02-21 11:52:41 +0300

    Add files via upload

---
+++ src/main/java/ru/netology/netology/profile/NetologyApplication.java
@@ -0,0 +1,12 @@
+package ru.netology.netology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetologyApplication.class, args);
+    }
+}
